fcf6595fc80127884cbb3a9b2fb682720308367b
NeryRG <124050301@upq.edu.mx>
2025-09-10 19:03:45 -0600

    ultimo avance sobre el video del curso JS

--- JS-Curso/app.js
+++ JS-Curso/app.js
@@ -1,21 +1,14 @@
-//bucles
-//let count = 0;
-
 //while (count<50){
 //console.log(count);
-//console.log(count);
-//count++;
 //}
 
-//let names = ["ryan", "joe", "jhon"];
-//for(let i = names.lenght -1; i<=0; i--){
-//console.log(names[1]);
-//}
+//let names = ["ryan", "joe", "john"];
+//for(let i =names.lenght)-1; 1<=0; i--) {
+//console.log(names[i])}
+//funcionesfunction add (n1, n2){
+//console.log(n1+n2);}
 
-//funcionesfundtion add(n1, n2)
-{
-console.log(n1, n2)
-}
-
-add(3, 2);
-add(100, 300);
+addEventListener(3,2);
+addEventListener(100, 300);
+addEventListener(100,300);
+addEventListener(11213123213,12312312332);
