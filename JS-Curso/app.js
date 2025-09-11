b851515c6f8c7b1cd5d06a659fd0ee5bf3ef900b
NeryRG <124050301@upq.edu.mx>
2025-09-10 18:59:11 -0600

    Cuarto commit sobre avace de practica

--- JS-Curso/app.js
+++ JS-Curso/app.js
@@ -1,1 +1,21 @@
-Document.write("<h1>Hello Word</h1>");
+//bucles
+//let count = 0;
+
+//while (count<50){
+//console.log(count);
+//console.log(count);
+//count++;
+//}
+
+//let names = ["ryan", "joe", "jhon"];
+//for(let i = names.lenght -1; i<=0; i--){
+//console.log(names[1]);
+//}
+
+//funcionesfundtion add(n1, n2)
+{
+console.log(n1, n2)
+}
+
+add(3, 2);
+add(100, 300);
